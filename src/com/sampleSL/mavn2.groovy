8d9a7e0b70b593d678b6c17b6bd52f36f45dbe66
Yuvaraj K <yuvaraj.vinay74@gmail.com>
2022-10-18 12:39:53 +0530

    demo

--- src/com/sampleSL/mavn2.groovy
+++ src/com/sampleSL/mavn2.groovy
@@ -1,23 +1,4 @@
 #!/usr/bin/env groovy
 package com.sampleSL
 
-def call(String name) {
-    pipeline {
-        agent any
-            stages {
-                stage ('SCM') {
-                    steps {
-                        echo "SCM URL, ${name}."
-                        git "${name}";
-                    }
-                    
-                }
-               stage ('Build') {
-                    steps {
-                        sh 'mvn clean package';
-                    }
-                    
-                } 
-            }
-        }
-    }
+
