d67a5fb36f486baaf0b0e85de320c0b24514a6c0
Yuvaraj K <yuvaraj.vinay74@gmail.com>
2022-10-18 14:40:16 +0530

    demo

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,22 +1,5 @@
 #!/usr/bin/env groovy
 
-def call(String name) {
-    pipeline {
-        agent any
-            stages {
-                stage ('SCM') {
-                    steps {
-                        echo "SCM URL, ${name}."
-                        git "${name}";
-                    }
-                    
-                }
-               stage ('Build') {
-                    steps {
-                        sh 'mvn clean package';
-                    }
-                    
-                } 
-            }
-        }
-    }
+def call() {
+      sh 'mvn clean package';
+      }
